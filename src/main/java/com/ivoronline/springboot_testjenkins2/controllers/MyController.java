0d450492a9257007686639bd76c0b9416ef80895
igoreta <ivoronline@gmail.com>
2021-02-17 21:30:18 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_testjenkins2/controllers/MyController.java
@@ -0,0 +1,16 @@
+package com.ivoronline.springboot_testjenkins2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+  @ResponseBody
+  @RequestMapping("/Hello")
+  public String hello() {
+    return "Hello from Controller";
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_testjenkins2/SpringbootTestjenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_testjenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestjenkins2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
