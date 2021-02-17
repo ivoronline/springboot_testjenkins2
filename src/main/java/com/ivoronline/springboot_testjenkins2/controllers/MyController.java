b6b9d660814baae22b974c434f1ff1c0052d0169
igoreta <ivoronline@gmail.com>
2021-02-17 21:49:09 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_testjenkins2/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_testjenkins2/controllers/MyController.java
@@ -10,7 +10,7 @@ public class MyController {
   @ResponseBody
   @RequestMapping("/Hello")
   public String hello() {
-    return "Hello from Controller1";
+    return "Hello from Controller12";
   }
 
 }
